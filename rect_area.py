34e97bf1c36b0a76766f1f6d6928542487223838
ujjwal singh baghel <ujjwalsingh15@gmail.com>
2018-02-21 16:58:45 +0530

    Small programs

---
+++ rect_area.py
@@ -0,0 +1,7 @@
+
+l = int(input("Enter the length of the rectangle: "))
+b = int(input("Enter the breadth of the rectangle: "))
+
+area = l*b
+
+print ("Area of the rectangle is: ", area)
---
+++ sq_root.py
@@ -0,0 +1,5 @@
+
+x = int(input('Enter any number: '))
+sq = x**0.5
+
+print ("Square root of ", x, " is ", sq)
