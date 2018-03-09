60196c5d9f06fc340a0751778c7bb97c38e42907
ujjwal singh <ujjwalsingh15@gmail.com>
2018-03-09 15:45:04 +0530

    completing nested dictionaries

---
+++ Full python (boring to interesting)/3_Dictionaries & Structuring Data/5_totalItemsInNestedDict.py
@@ -0,0 +1,20 @@
+
+
+allGuests = {'alice':{'apples':5, 'pretzels':12},
+			'Bob':{'ham sandwhiches':3, 'apples':2},
+			'Carol':{'cups':3, 'apple pies':1}
+			}
+
+def totalBrought(guests, item):
+	numBrought = 0
+	for k, v in guests.items():
+		numBrought = numBrought + v.get(item, 0)
+	return numBrought
+
+print('Number of things being brought:')
+
+print(' - Apples ' + str(totalBrought(allGuests, 'apples')))
+print(' - Cups ' + str(totalBrought(allGuests, 'cups')))
+print(' - Cakes ' + str(totalBrought(allGuests, 'cakes')))
+print(' - Ham Sandwhiches ' + str(totalBrought(allGuests, 'ham sandwhiches')))
+print(' - apple pies ' + str(totalBrought(allGuests, 'apple pies'))) 
