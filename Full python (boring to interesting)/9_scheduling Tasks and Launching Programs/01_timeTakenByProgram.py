ed70d35eb9abb72fc8dabee081d6dc0d9e5158e5
ujjwal singh <ujjwalsingh15@gmail.com>
2018-03-15 16:28:30 +0530

    Working on time scheduling tasks

---
+++ Full python (boring to interesting)/9_scheduling Tasks and Launching Programs/01_timeTakenByProgram.py
@@ -0,0 +1,15 @@
+import time
+
+def calcProd():
+	# Calculate product of first 100,000 numbers.
+	product = 1
+	for i in range (1, 100000):
+		product = product*i
+	return product
+
+startTime = time.time()
+prod = calcProd()
+endTime = time.time()
+
+print('The result is %s digits long.' %(len(str(prod))))
+print('Took %s seconds to Calculate.' %(endTime - startTime))
---
+++ Full python (boring to interesting)/9_scheduling Tasks and Launching Programs/02_sleepFunction.py
@@ -0,0 +1,9 @@
+import time
+
+for i in range(3):
+	print('Tick')
+	time.sleep(1)
+	print('Tock')
+	time.sleep(1)
+
+time.sleep(5)
